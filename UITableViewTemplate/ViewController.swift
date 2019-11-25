17987fe16ad89c0f89c8b39e590b96863ced2dc0
Paola Latino <paolalat188@gmail.com>
2019-11-24 21:10:47 -0500

    First Commit
    Created basic structure in View Controller and updated storyboard

---
+++ UITableViewTemplate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITableViewTemplate
+//
+//  Created by Paola Latino on 11/24/19.
+//  Copyright © 2019 Paola Latino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
